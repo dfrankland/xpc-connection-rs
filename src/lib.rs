4d31af1e5f1b2f2caaa5afd6e117dd3163736c9c
Dylan Frankland <dylan@frankland.io>
2018-09-17 22:54:55 -0700

    Add async messaging deque

--- src/lib.rs
+++ src/lib.rs
@@ -5,63 +5,71 @@
 pub mod message;
 pub mod xpc_sys;
 
-use std::{boxed::Box, ffi::CString, os::raw::c_void};
+use std::{
+    boxed::Box,
+    ffi::CString,
+    os::raw::c_void,
+    thread,
+};
+
+use crossbeam_deque::{fifo, Steal};
 
 use self::{
-    message::Message,
+    message::{Message, xpc_object_to_message},
     xpc_sys::{
         dispatch_queue_attr_s, dispatch_queue_create, xpc_connection_create_mach_service,
         xpc_connection_resume, xpc_connection_send_message, xpc_connection_set_event_handler,
-        xpc_connection_t, xpc_object_t, xpc_release, xpc_retain,
+        xpc_connection_t, xpc_object_t, xpc_release,
         XPC_CONNECTION_MACH_SERVICE_PRIVILEGED,
     },
 };
 
-pub struct XpcConnection<T: Fn(Message) -> () + Send> {
-    service_name: String,
-    callback: Option<T>,
+pub struct XpcConnection {
+    pub service_name: String,
     connection: Option<xpc_connection_t>,
 }
 
-impl<T: Fn(Message) -> () + Send> XpcConnection<T> {
-    pub fn new(service_name: &str) -> XpcConnection<T> {
+impl XpcConnection {
+    pub fn new(service_name: &str) -> XpcConnection {
         XpcConnection {
             service_name: service_name.to_owned(),
-            callback: None,
             connection: None,
         }
     }
 
-    pub fn set_callback(self: &mut Self, callback: T) {
-        self.callback = Some(callback);
-    }
+    pub fn setup<T: 'static + Fn(Message) + Send>(self: &mut Self, callback: T) {
+        // Setup a worker to asynchronously check for messages
+        let (w, s) = fifo();
+        thread::spawn(move || {
+            loop {
+                if let Steal::Data(message) = s.steal() {
+                    callback(message);
+                }
+            }
+        });
 
-    pub fn setup(self: &mut Self) {
+        // Start a connection
         let label_name = CString::new(self.service_name.clone()).unwrap();
-        let attr = Box::into_raw(Box::new(dispatch_queue_attr_s { _address: 0 }));
-        let dispatch_queue = unsafe { dispatch_queue_create(label_name.as_ptr(), attr) };
-
         let connection = unsafe {
             xpc_connection_create_mach_service(
                 label_name.as_ptr(),
-                dispatch_queue,
+                dispatch_queue_create(
+                    label_name.as_ptr(),
+                    Box::into_raw(Box::new(dispatch_queue_attr_s { _address: 0 }))
+                ),
                 u64::from(XPC_CONNECTION_MACH_SERVICE_PRIVILEGED),
             )
         };
-
         self.connection = Some(connection);
 
-        let mut cb: &mut FnMut(xpc_object_t) = &mut move |event| {
-            unsafe {
-                xpc_retain(event);
-                // TODO: Call one of the callbacks
-                xpc_release(event);
-            }
+        // Handle messages received
+        let mut event_handler: &mut FnMut(xpc_object_t) = &mut move |event| {
+            w.push(xpc_object_to_message(event));
+            unsafe { xpc_release(event); }
         };
-        let cb = &mut cb;
-
+        let event_handler = &mut event_handler;
         unsafe {
-            xpc_connection_set_event_handler(connection, cb as *mut _ as *mut c_void);
+            xpc_connection_set_event_handler(connection, event_handler as *mut _ as *mut c_void);
             xpc_connection_resume(connection);
         }
     }
