bd3d4eb2b0cb3d2728f61b50c5025ff008e8d6d7
Geoff Cant <nem@erlang.geek.nz>
2019-03-17 17:02:44 -0700

    Limit bindgen to necessary ffi functions only.

--- xpc-connection-sys/build.rs
+++ xpc-connection-sys/build.rs
@@ -6,6 +6,13 @@ fn main() {
 
     bindgen::Builder::default()
         .header("wrapper.h")
+        .rustfmt_bindings(true)
+        .whitelist_function("dispatch_queue_create")
+        .whitelist_function("xpc.*")
+        .whitelist_var("xpc.*")
+        .whitelist_var("_xpc.*")
+        .whitelist_var("XPC.*")
+        .whitelist_type("uuid_t")
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(out_path.join("bindings.rs"))
